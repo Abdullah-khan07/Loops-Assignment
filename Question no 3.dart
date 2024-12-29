7d16e0a5e35a3e04cceae9cf35dcbd10b5d8ccfb
Abdullah-khan07 <abdullahahmedkhan072@gmail.com>
2024-12-29 02:36:50 -0800

    list sum

--- Question no 3.dart
+++ Question no 3.dart
@@ -2,9 +2,10 @@ void main() {
   //calculates the sum
   List<int> number = [1, 2, 3, 4, 5];
   int sum = 0;
-  for (var number in number) {
-    sum += number;
-    
+  int i = 0;
+  while (i < number.length) {
+    sum += number[i];
+    i++;
   }
   print(sum);
 }
