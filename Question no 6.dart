0e6c0ee6d1c978cfd36ba89aba26037b0e9919f3
Abdullah-khan07 <abdullahahmedkhan072@gmail.com>
2025-01-01 17:46:14 -0800

    Answer is Number of vowels: 8

--- Question no 6.dart
+++ Question no 6.dart
@@ -1,4 +1,17 @@
 void main (){
+  ///Write a program that counts the number of vowels in a given string
+//using a for loop and if-else condition
+String text = "Dart programming language";
+int vowelCount = 0;
+String vowels = "aeiouAEIOU";
+for (int i = 0; i < text.length; i++) {
+  if (vowels.contains(text[i])) {
+    vowelCount++;
+  }
+}
+print("Number of vowels: $vowelCount");
+
+
 
 }
   
