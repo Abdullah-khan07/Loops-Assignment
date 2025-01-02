7914e2b58720294610a2be66c5c32d14daf15836
Abdullah-khan07 <abdullahahmedkhan072@gmail.com>
2025-01-01 18:16:15 -0800

    Number of rows for the pattern

---
+++ Question no 14.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main() {
+  // //Write a program to display a pattern like a right angle triangle with a
+///number using loop.
+  int rows = 4; // Number of rows for the pattern
+  
+  for (int i = 1; i <= rows; i++) {
+    for (int j = 1; j <= i; j++) {
+      stdout.write(i);
+    }
+    print('');
+  }
+}
